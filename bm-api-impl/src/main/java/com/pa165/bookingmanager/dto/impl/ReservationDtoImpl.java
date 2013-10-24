4e54f18e8c6caddd38a98f7a6765d641b24d3675
Josef Strzibny <strzibny@strzibny.name>
2013-10-24 21:28:04 +0200

    Copy dto implementation into bm-api-impl and let it implement respective interfaces

---
+++ bm-api-impl/src/main/java/com/pa165/bookingmanager/dto/impl/ReservationDtoImpl.java
@@ -0,0 +1,230 @@
+package com.pa165.bookingmanager.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author Josef Stribny
+ */
+public class ReservationDtoImpl  implements ReservationDto, Serializable
+{
+    /**
+     * Id
+     */
+    private Long id;
+
+    /**
+     * Rooms by room id
+     */
+    private RoomDto roomByRoomId;
+
+    /**
+     * Reservation from
+     */
+    private Date reservationFrom;
+
+    /**
+     * Reservation to
+     */
+    private Date reservationTo;
+
+    /**
+     * Customer name
+     */
+    private String customerName;
+
+    /**
+     * Customer email
+     */
+    private String customerEmail;
+
+    /**
+     * Customer phone
+     */
+    private String customerPhone;
+
+    /**
+     * Get id
+     *
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Set id
+     *
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Get room by room id
+     *
+     * @return room by room id
+     */
+    public RoomDto getRoomByRoomId() {
+        return roomByRoomId;
+    }
+
+    /**
+     * Set room by room id
+     *
+     * @param roomByRoomId room by room id
+     */
+    public void setRoomByRoomId(RoomDto roomByRoomId) {
+        this.roomByRoomId = roomByRoomId;
+    }
+
+    /**
+     * Get reservation from
+     *
+     * @return reservation from
+     */
+    public Date getReservationFrom() {
+        return reservationFrom;
+    }
+
+    /**
+     * Set reservation from
+     *
+     * @param reservationFrom reservation from
+     */
+    public void setReservationFrom(Date reservationFrom) {
+        this.reservationFrom = reservationFrom;
+    }
+
+    /**
+     * Get reservation to
+     *
+     * @return reservation to
+     */
+    public Date getReservationTo() {
+        return reservationTo;
+    }
+
+    /**
+     * Set reservation to
+     *
+     * @param reservationTo reservation to
+     */
+    public void setReservationTo(Date reservationTo) {
+        this.reservationTo = reservationTo;
+    }
+
+    /**
+     * Get customer name
+     *
+     * @return customer name
+     */
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    /**
+     * Set customer name
+     *
+     * @param customerName customer name
+     */
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    /**
+     * Get customer email
+     *
+     * @return customer email
+     */
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    /**
+     * Set customer email
+     *
+     * @param customerEmail customer email
+     */
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    /**
+     * Get customer phone
+     *
+     * @return customer phone
+     */
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    /**
+     * Set customer phone
+     *
+     * @param customerPhone customer phone
+     */
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    /**
+     * Equals
+     *
+     * @param o object
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ReservationDto)) return false;
+
+        ReservationDto that = (ReservationDto) o;
+
+        if (!customerEmail.equals(that.customerEmail)) return false;
+        if (!customerName.equals(that.customerName)) return false;
+        if (!customerPhone.equals(that.customerPhone)) return false;
+        if (!id.equals(that.id)) return false;
+        if (!reservationFrom.equals(that.reservationFrom)) return false;
+        if (!reservationTo.equals(that.reservationTo)) return false;
+        if (!roomByRoomId.equals(that.roomByRoomId)) return false;
+
+        return true;
+    }
+
+    /**
+     * Hash code
+     *
+     * @return hash code
+     */
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + roomByRoomId.hashCode();
+        result = 31 * result + reservationFrom.hashCode();
+        result = 31 * result + reservationTo.hashCode();
+        result = 31 * result + customerName.hashCode();
+        result = 31 * result + customerEmail.hashCode();
+        result = 31 * result + customerPhone.hashCode();
+        return result;
+    }
+
+    /**
+     * To string
+     *
+     * @return string representation of object
+     */
+    @Override
+    public String toString() {
+        return "ReservationDto{" +
+                "id=" + id +
+                ", roomByRoomId=" + roomByRoomId +
+                ", reservationFrom=" + reservationFrom +
+                ", reservationTo=" + reservationTo +
+                ", customerName='" + customerName + '\'' +
+                ", customerEmail='" + customerEmail + '\'' +
+                ", customerPhone='" + customerPhone + '\'' +
+                '}';
+    }
+}
---
+++ bm-api-impl/src/main/java/com/pa165/bookingmanager/dto/impl/UserDtoImpl.java
@@ -0,0 +1,151 @@
+package com.pa165.bookingmanager.dto;
+
+import java.io.Serializable;
+
+/**
+ * @author Josef Stribny
+ */
+public class UserDtoImpl implements UserDto, Serializable
+{
+    /**
+     * Id
+     */
+    private Long id;
+
+    /**
+     * Email
+     */
+    private String email;
+
+    /**
+     * Password
+     */
+    private String password;
+
+    /**
+     * Role by role id
+     */
+    private RoleDto roleByRoleId;
+
+    /**
+     * Get id
+     *
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Set id
+     *
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Get email
+     *
+     * @return email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Set email
+     *
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Get password
+     *
+     * @return password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Set password
+     *
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Get role by role id
+     *
+     * @return role by role id
+     */
+    public RoleDto getRoleByRoleId() {
+        return roleByRoleId;
+    }
+
+    /**
+     * Set role by role id
+     *
+     * @param roleByRoleId role by role id
+     */
+    public void setRoleByRoleId(RoleDto roleByRoleId) {
+        this.roleByRoleId = roleByRoleId;
+    }
+
+    /**
+     * Equals
+     *
+     * @param o object
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserDto)) return false;
+
+        UserDto userDto = (UserDto) o;
+
+        if (!email.equals(userDto.email)) return false;
+        if (!id.equals(userDto.id)) return false;
+        if (!password.equals(userDto.password)) return false;
+        if (!roleByRoleId.equals(userDto.roleByRoleId)) return false;
+
+        return true;
+    }
+
+    /**
+     * Hash code
+     *
+     * @return hash code
+     */
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + email.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + roleByRoleId.hashCode();
+        return result;
+    }
+
+    /**
+     * To string
+     *
+     * @return string representation of object
+     */
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", roleByRoleId=" + roleByRoleId +
+                '}';
+    }
+}

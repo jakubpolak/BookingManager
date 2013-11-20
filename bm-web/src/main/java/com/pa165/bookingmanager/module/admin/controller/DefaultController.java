34593024f7cc8ba6f5e1e13ff4654afb0be22bdf
Jakub Polák <jakub.polak@me.com>
2013-11-21 00:13:50 +0100

    Add changing of language automatically according to user browser preferences.

--- bm-web/src/main/java/com/pa165/bookingmanager/module/admin/controller/DefaultController.java
+++ bm-web/src/main/java/com/pa165/bookingmanager/module/admin/controller/DefaultController.java
@@ -1,7 +1,8 @@
 package com.pa165.bookingmanager.module.admin.controller;
 
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,18 +19,19 @@ public class DefaultController
      * @return redirect to /admin/hotel/list-of-hotels
      */
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index() {
-        return "redirect:/admin/hotel/list-of-hotels";
-    }
+    public String index(@RequestHeader(value = "Accept-Language") String lang) {
+        // Get current display language
+        String displayLanguage = LocaleContextHolder.getLocale().getDisplayLanguage();
 
-    /**
-     * Change language
-     *
-     * @param lang lang
-     * @return redirect to /admin
-     */
-    @RequestMapping(value = "/{lang}/change-language", method = RequestMethod.GET)
-    public String changeLanguage(@PathVariable String lang){
-        return "redirect:/admin";
+        // Change language automatically according to user browser preferences
+        // It can be changed manually by calling ?lang=sk or ?lang=en, but it will change back to browser preferences
+        // when user comes back to /admin
+        if (lang.contains("sk-") && !displayLanguage.equals("Slovak")){
+            return "redirect:/admin/hotel/list-of-hotels?lang=sk";
+        } else if (lang.contains("en-") && !displayLanguage.equals("English")) {
+            return "redirect:/admin/hotel/list-of-hotels?lang=en";
+        }
+
+        return "redirect:/admin/hotel/list-of-hotels";
     }
 }
